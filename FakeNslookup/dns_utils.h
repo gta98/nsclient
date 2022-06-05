b324e9706986f4a8199dddd912e9d2819968f2d2
gta98 <me@glub.xyz>
2022-06-05 12:05:26 +0300

    Moved structs into header

--- FakeNslookup/dns_utils.h
+++ FakeNslookup/dns_utils.h
@@ -4,6 +4,38 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "common_includes.h"
 
+struct dns_header {
+    unsigned short id;
+    unsigned char rd : 1;
+    unsigned char tc : 1;
+    unsigned char aa : 1;
+    unsigned char opcode : 4;
+    unsigned char qr : 1;
+    unsigned char rcode : 4;
+    unsigned char cd : 1;
+    unsigned char ad : 1;
+    unsigned char z : 1;
+    unsigned char ra : 1;
+
+    unsigned short q_count;
+    unsigned short ans_count;
+    unsigned short auth_count;
+    unsigned short add_count;
+};
+
+
+struct question {
+    unsigned short qtype;
+    unsigned short qclass;
+};
+
+
+typedef struct {
+    unsigned char* name;
+    struct question* ques;
+}; query;
+
+
 SOCKET sock;
 
 int change_question_name(const char* hostname, unsigned char* qname);
